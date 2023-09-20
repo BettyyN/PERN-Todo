8e89acd690fdbfa61f9edc86899c3517ad796545
BettyyN <betelhemnegash10@gmail.com>
2023-09-20 10:27:25 -0700

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import './App.css';
+
+// components
+    
+
+
+function App() {
+  return (
+    <Fragment></Fragment>
+  );
+}
+
+export default App;
---
+++ client/src/components/InputTodo.js
@@ -0,0 +1,6 @@
+import React from "react";
+const InputTodo = () =>{
+    return <h1> Input Todo</h1>
+
+}; 
+export default InputTodo;
