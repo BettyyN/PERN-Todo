c57457d2ec56d0e55ce3ba66bf52ab382d72b406
BettyyN <betelhemnegash10@gmail.com>
2023-10-05 16:18:26 -0700

    some changes in modal

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -31,30 +31,26 @@ data-target={`#id${todo.todo_id}`}
 id=id10
 */}
 
-<div class="modal" id={`id${todo.todo_id}`} >
+<div class="modal" id={`id${todo.todo_id}`} onClick={() => setDescription(todo.description)}>
   <div class="modal-dialog">
     <div class="modal-content">
 
       <div class="modal-header">
         <h4 class="modal-title">Edit Todo</h4>
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <button type="button" class="close" data-dismiss="modal" 
+        onClick={() => setDescription(todo.description)}>&times;</button>
       </div>
 
       
       <div class="modal-body">
         <input type="text" className="form-control" value={description} 
         onChange={e=> setDescription(e.target.value)}/>
-{/*         
-        <input type="text"
-            className="form-control"
-            value={description || ""} // Use an empty string if description is null
-            onChange={(e) => setDescription(e.target.value)}
-/> */}
+
       </div>
 
       <div class="modal-footer">
       <button type="button" class="btn btn-warning" data-dismiss="modal" onClick={ e=> updateDescription(e)}>Edit</button>
-        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-danger" data-dismiss="modal" onClick={() => setDescription(todo.description)}>Close</button>
       </div>
 
     </div>
